92429781f5181f63e9f499de39fc3cf00fda96a7
Ethan Paul <24588726+enpaul@users.noreply.github.com>
2022-05-30 23:02:46 -0400

    Restructure package for directory module structure

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -7,7 +7,7 @@ from pathlib import Path
 
 import toml
 
-import glassy
+from glassy import __about__
 
 
 def test_metadata():
@@ -16,14 +16,14 @@ def test_metadata():
     with (Path(__file__).resolve().parent / ".." / "pyproject.toml").open() as infile:
         pyproject = toml.load(infile, _dict=dict)
 
-    assert pyproject["tool"]["poetry"]["name"] == glassy.__title__
-    assert pyproject["tool"]["poetry"]["version"] == glassy.__version__
-    assert pyproject["tool"]["poetry"]["license"] == glassy.__license__
-    assert pyproject["tool"]["poetry"]["description"] == glassy.__summary__
-    assert pyproject["tool"]["poetry"]["repository"] == glassy.__url__
+    assert pyproject["tool"]["poetry"]["name"] == __about__.__title__
+    assert pyproject["tool"]["poetry"]["version"] == __about__.__version__
+    assert pyproject["tool"]["poetry"]["license"] == __about__.__license__
+    assert pyproject["tool"]["poetry"]["description"] == __about__.__summary__
+    assert pyproject["tool"]["poetry"]["repository"] == __about__.__url__
     assert (
         all(
-            item in glassy.__authors__
+            item in __about__.__authors__
             for item in pyproject["tool"]["poetry"]["authors"]
         )
         is True
@@ -31,7 +31,7 @@ def test_metadata():
     assert (
         all(
             item in pyproject["tool"]["poetry"]["authors"]
-            for item in glassy.__authors__
+            for item in __about__.__authors__
         )
         is True
     )
